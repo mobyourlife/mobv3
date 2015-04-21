856d80f12a423ecc399d6f75d6394b475dd6fb9e
Filipe Oliveira <contato@fmoliveira.com.br>
2015-04-21 06:01:47 -0300

    Melhorando a fórmula dos backgrounds aleatórios.

--- core/controllers/index.js
+++ core/controllers/index.js
@@ -6,7 +6,7 @@ module.exports = function (router) {
 
     router.get('/', function (req, res) {
         var model = currentLanguage(req);
-        model.background = 'bg-' + (Math.round((Date.now() / 1000) / 3600) % 15);
+        model.background = 'bg-' + ((Math.round((Date.now() / 1000) / 1800) % 15) + 1);
         res.render('index', model);
     });
 
