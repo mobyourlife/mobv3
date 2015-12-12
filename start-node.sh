4bec35e95f7463a6dc2129f0794f11c21f4fa57c
Filipe Oliveira <contato@fmoliveira.com.br>
2015-12-12 18:51:53 +0000

    Dockerfile para execução do background sync.

---
+++ start-node.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+docker rm -f mobv3
+
+#docker rmi -f mobv3
+
+#docker build -t mobv3 .
+
+docker run \
+	--name mobv3 \
+	--restart=always \
+	--link mob-mongo:db \
+	-e FACEBOOK_CLIENT_ID=$FACEBOOK_CLIENT_ID \
+	-e FACEBOOK_CLIENT_SECRET=$FACEBOOK_CLIENT_SECRET \
+	-d \
+	mobv3
